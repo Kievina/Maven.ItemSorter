036230c91319ed2b1151b42feadf8b5c17fc606b
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-03 19:51:21 -0500

    complete; all tests pass

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,34 @@
 package rocks.zipcode.io;
 
+import rocks.zipcode.io.comparators.IdComparator;
+import rocks.zipcode.io.comparators.NameComparator;
+import rocks.zipcode.io.comparators.PriceComparator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    private Item[] items;
+
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        ArrayList<Item> itemArrayList = new ArrayList<Item>(Arrays.asList(items));
+        int arrayLength = items.length;
+        if (comparator.equals(new IdComparator())) {
+            Collections.sort(itemArrayList, new IdComparator());
+        } else if (comparator.equals(new NameComparator())) {
+            Collections.sort(itemArrayList, new NameComparator());
+        } else {
+            Collections.sort(itemArrayList, new PriceComparator());
+        }
+        return itemArrayList.toArray(new Item[arrayLength]);
     }
 }
